02373253921d3e054e3bb98451fe58f22aadb828
“Shubham” <shubhampattar2@gmail.com>
2020-08-02 02:42:54 +0530

    ADDED JS Code To Compute Sum And Average For 5 Random 2 Digit Numbers.

---
+++ randomSumAverage.js
@@ -0,0 +1,9 @@
+let num1 = (Math.floor(Math.random()*2)+1);
+let num2 = (Math.floor(Math.random()*2)+1);
+let num3 = (Math.floor(Math.random()*2)+1);
+let num4 = (Math.floor(Math.random()*2)+1);
+let num5 = (Math.floor(Math.random()*2)+1);
+let sum = num1+num2+num3+num4+num5;
+let average = sum / 5;
+console.log("Sum of 5 Random 2 Digit Numbers = "+sum);
+console.log("Average of 5 Random 2 Digit Numbers = "+average);
