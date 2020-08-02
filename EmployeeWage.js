4a03c54995d37db53938e4497d9144612a95d10d
“Shubham” <shubhampattar2@gmail.com>
2020-08-02 15:04:50 +0530

    ADDED Code To Get Employee Working Hours Using Function.

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -12,19 +12,24 @@ let empHrs = 0, empWage = 0;
 let totalEmpHrs = 0;
 let totalWorkingDays = 0;
 
+function getWorkingHours(empCheck){
+    switch(empCheck) {
+        case IS_PART_TIME:
+            empHrs = 4;
+            break
+        case IS_FULL_TIME:
+            empHrs = 8;
+            break
+        default:
+            empHrs = 0;
+        }
+    return empHrs;
+}
+
 while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <= NUM_OF_WORKING_DAYS) {
     totalWorkingDays++;
     let empCheck = Math.floor(Math.random() * 10) % 3;
-    switch(empCheck) {
-    case IS_PART_TIME:
-        empHrs = 4;
-        break
-    case IS_FULL_TIME:
-        empHrs = 8;
-        break
-    default:
-        empHrs = 0;
-    }
+    empHrs = getWorkingHours(empCheck);
     totalEmpHrs += empHrs;
 }
 
