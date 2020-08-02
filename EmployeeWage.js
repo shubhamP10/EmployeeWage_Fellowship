318829e7795008d9cf2354216a9382ffdd927bc7
“Shubham” <shubhampattar2@gmail.com>
2020-08-02 14:15:41 +0530

    ADDED Code To Add Part Time Employee And Wage.

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,11 +1,15 @@
 // Constants
 let EMP_RATE_PER_HOUR = 20;
+let IS_PART_TIME = 1;
+let IS_FULL_TIME = 2;
 
 // Variables
 let isPresent = 1;
-let empCheck = Math.floor(Math.random() * 10) % 2;
+let empCheck = Math.floor(Math.random() * 10) % 3;
 let empHrs = 0, empWage = 0;
-if (empCheck == 1)
+if (empCheck == IS_PART_TIME)
+    empHrs = 4;
+else if (empCheck == IS_FULL_TIME)
     empHrs = 8;
 else
     empHrs = 0;
