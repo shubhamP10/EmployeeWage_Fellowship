6c111a26cb2a1b9f5f42e82b2fe886bf88a28019
“Shubham” <shubhampattar2@gmail.com>
2020-08-02 14:22:34 +0530

    REFACTORED Code By Using Switch Case.

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -7,12 +7,18 @@ let IS_FULL_TIME = 2;
 let isPresent = 1;
 let empCheck = Math.floor(Math.random() * 10) % 3;
 let empHrs = 0, empWage = 0;
-if (empCheck == IS_PART_TIME)
-    empHrs = 4;
-else if (empCheck == IS_FULL_TIME)
-    empHrs = 8;
-else
-    empHrs = 0;
+
+switch(empCheck){
+    case IS_PART_TIME:
+        empHrs = 4;
+        break
+    case IS_FULL_TIME:
+        empHrs = 8;
+        break
+    default:
+        empHrs = 0;
+}
+
 empWage = empHrs * EMP_RATE_PER_HOUR;
 console.log("Employee Wage = "+empWage);
 
