a84c034c04529c46823e5abac2e49a19b1c54816
“Shubham” <shubhampattar2@gmail.com>
2020-08-02 14:59:28 +0530

    ADDED Code To Calculate Monthly Wage Based On Condition.

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -3,13 +3,17 @@ let EMP_RATE_PER_HOUR = 20;
 let IS_PART_TIME = 1;
 let IS_FULL_TIME = 2;
 let NUM_OF_WORKING_DAYS = 20;
+let MAX_HRS_IN_MONTH = 100;
 
 // Variables
 let isPresent = 1;
 let monthlyWage = 0;
 let empHrs = 0, empWage = 0;
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
 
-for(let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <= NUM_OF_WORKING_DAYS) {
+    totalWorkingDays++;
     let empCheck = Math.floor(Math.random() * 10) % 3;
     switch(empCheck) {
     case IS_PART_TIME:
@@ -21,9 +25,9 @@ for(let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
     default:
         empHrs = 0;
     }
-    empWage = empHrs * EMP_RATE_PER_HOUR;
-    monthlyWage += empWage;
+    totalEmpHrs += empHrs;
 }
 
+monthlyWage = totalEmpHrs * EMP_RATE_PER_HOUR;
 console.log("Employee Monthly Wage = " + monthlyWage);
 
