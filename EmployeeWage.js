ea542e938988d5009d8e9396a1aaed4211bc750f
“Shubham” <shubhampattar2@gmail.com>
2020-08-02 14:06:51 +0530

    ADDED Code To Compute Daily Employee Wage.

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,6 +1,14 @@
+// Constants
+let EMP_RATE_PER_HOUR = 20;
+
+// Variables
 let isPresent = 1;
 let empCheck = Math.floor(Math.random() * 10) % 2;
+let empHrs = 0, empWage = 0;
 if (empCheck == 1)
-    console.log("Employee is Present");
+    empHrs = 8;
 else
-    console.log("Employee is Abscent");
+    empHrs = 0;
+empWage = empHrs * EMP_RATE_PER_HOUR;
+console.log("Employee Wage = "+empWage);
+
